10518903a9e2816be364925e196cf151bfc0bc1e
Mafusen <mb577583@gmail.com>
2021-10-11 16:34:37 +0200

    Opprettet addToolServlet.java

---
+++ src/main/java/bacit/web/bacit_web/addToolServlet.java
@@ -0,0 +1,19 @@
+package bacit.web.bacit_web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "addToolServlet", value = "addToolServlet")
+public class addToolServlet extends HttpServlet{
+    @Override 
+}
