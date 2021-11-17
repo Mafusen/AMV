3d345d51b8623aef5893e58de88580d4d7cffbf3
oorezz <67963850+oorezz@users.noreply.github.com>
2021-11-17 10:02:40 +0100

    La til koden

--- src/main/java/bacit/web/Web/JSP_connecters/popupBookingFailed.java
+++ src/main/java/bacit/web/Web/JSP_connecters/popupBookingFailed.java
@@ -1,2 +1,19 @@
-package bacit.web.Web.JSP_connecters;public class popupBookingFailed {
+package bacit.web.Web.JSP_connecters;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet (name = "popupBooking", value = "/popupBooking")
+public class popupBookingFailed extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/popupBookingFailed.jsp").forward(request, response);
+
+    }
 }
