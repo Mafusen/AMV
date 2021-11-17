f95cd8a51cee2c8258db488c9bb87b4bd8b5fd38
oorezz <67963850+oorezz@users.noreply.github.com>
2021-11-17 10:04:48 +0100

    la til deleteCertificate metoden

--- src/main/java/bacit/web/DAOs/CertificateDAO.java
+++ src/main/java/bacit/web/DAOs/CertificateDAO.java
@@ -12,6 +12,8 @@ public class CertificateDAO {
 
     PrintWriter out;
 
+
+    //Register certificate and if already found update the date!
     public void registerCertificate(CertificateModel certificate){
 
         try{
@@ -20,13 +22,14 @@ public class CertificateDAO {
             Connection db = DBUtils.getINSTANCE().getConnection(out);
 
             // Write  insertion query
-            String query = "insert into CERTIFICATE (USER_ID, COURSE_ID, Expiry_Date) values (?, ?, ?)";
+            String query = "insert into CERTIFICATE (USER_ID, COURSE_ID, Expiry_Date) values (?, ?, ?) ON DUPLICATE KEY UPDATE Expiry_Date = ?";
 
             // Set parameters with PreparedStatement
             PreparedStatement statement = db.prepareStatement(query);
             statement.setInt(1, certificate.getUserID());
             statement.setInt(2, certificate.getCourseID());
             statement.setDate(3, certificate.getDate());
+            statement.setDate(4, certificate.getDate());
 
             // Execute the statement
             statement.executeQuery();
@@ -37,4 +40,27 @@ public class CertificateDAO {
 
     }
 
+    public void deleteCertificate(CertificateModel certificate){
+
+        try{
+
+            // Get connection to database
+            Connection db = DBUtils.getINSTANCE().getConnection(out);
+
+            // Write  insertion query
+            String query =
+                    "delete from CERTIFICATE where User_ID = ?";
+
+            // Set parameters with PreparedStatement
+            PreparedStatement statement = db.prepareStatement(query);
+            statement.setInt(1, certificate.getUserID());
+
+            // Execute the statement
+            statement.executeQuery();
+
+        }catch (SQLException | ClassNotFoundException exception){
+            exception.printStackTrace();
+        }
+
+    }
 }
