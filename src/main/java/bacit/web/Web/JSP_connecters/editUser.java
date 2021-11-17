3d987e70ad15ead9e7eac1ca95563dbff2ff4609
oorezz <67963850+oorezz@users.noreply.github.com>
2021-11-17 10:01:41 +0100

    La til koden

--- src/main/java/bacit/web/Web/JSP_connecters/editUser.java
+++ src/main/java/bacit/web/Web/JSP_connecters/editUser.java
@@ -1,2 +1,19 @@
-package bacit.web.Web.JSP_connecters;public class editUser {
+package bacit.web.Web.JSP_connecters;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet (name = "/admin/editUser", value = "/admin/editUser")
+public class editUser extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/editUser.jsp").forward(request, response);
+
+    }
 }
